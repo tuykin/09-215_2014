53c2f84d70fb475b8c9691364c041ffafe6f0347
Adel Nabiullin <throughyourhead@gmail.com>
2014-10-06 21:54:19 +0400

    Update Program3.java

--- 0-basics/nabiullin_adel/3/Program3.java
+++ 0-basics/nabiullin_adel/3/Program3.java
@@ -5,46 +5,30 @@ class Program3 {
     public static void main(String[] args) {
         String[] myStrings;
         try {
-            BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
             System.out.println("Enter the number of strings you want:");
-            int quantity = 0;
-            // дальше идет считывание количества{string} строк и
-            // конвертирование его в char[], потом в int
-            String myStr = buf.readLine();
-            int strLength = myStr.length();
-            char[] myCMass = myStr.toCharArray();
-            int i = 0, j = strLength;
-            while(i<strLength) {
-                int k = 1;
-                for(int p = 0; p < j-1; p++){
-                    k*=10;
-                }
-                j--;
-                quantity+=k*((int)myCMass[i]-(int)'0');
-                i++;
-            }
+            Scanner sc = new Scanner(System.in);
+            int quantity = sc.nextInt();
             myStrings = new String[quantity];
-            // будем считывать строки в myStrings из консоли
             System.out.println("On the every line enter: \"name first_num second_num\" through gaps:");
-            for(i = 0; i < quantity; i++) {
+            BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+            for(int i = 0; i < quantity; i++) {
                 myStrings[i] = buf.readLine();
             }
-            int w = 0;
-            double firstNum = 0 , secondNum = 0;
-            for(i = 0; i < quantity; i++) {
+            int firstNum = 0 , secondNum = 0;
+            for(int i = 0; i < quantity; i++) {
                 int t = 0;
-                for(w = 0; w < 81; w++)
+                for(int w = 0; w < 81; w++)
                     System.out.print('-');
                 System.out.print('\n');
                 StringTokenizer st = new StringTokenizer(myStrings[i]);
                 while(st.hasMoreTokens()) {
                     System.out.print('|');
                     String forReading = st.nextToken();
-                    strLength = forReading.length();
+                    int strLength = forReading.length();
                     System.out.printf("%19s", forReading);
-                    myCMass = forReading.toCharArray();
-                    double Num = 0;
+                    int Num = 0;
                     if(t==1||t==2) {
+                        char[] myCMass = forReading.toCharArray();
                         int r  = 0, q = strLength;
                         while(r<strLength) {
                             int k = 1;
@@ -63,11 +47,9 @@ class Program3 {
                     t++;
                 }
                 System.out.print("|");
-                // "%.3f" - чтобы было 3 знака после запятой, но не дает юзать сразу 2 '%'
-                System.out.printf("%19f",firstNum/secondNum);
-                System.out.println("|");
+                System.out.printf("%19.3f|\n",(double)firstNum/secondNum);
             }
-            for(w = 0; w < 81; w++)
+            for(int w = 0; w < 81; w++)
                 System.out.print('-');
         }
         catch(Exception ourException) {
