ed92081d566665bd534cd25d61a9a3d39e0b06d7
amirka369 <amirka369@gmail.com>
2014-10-07 23:32:52 +0400

    Create program2.java

---
+++ 0-basics/Sadykov_Amir/program2.java
@@ -0,0 +1,22 @@
+package work1;
+
+import java.util.Scanner;
+
+public class program2 {
+	public static void main(String[] args) {
+		
+		Scanner scan=new Scanner(System.in);
+		String str= scan.nextLine();
+		String strCopy=new String("");
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			strCopy=strCopy+str.charAt(i);
+		
+		}
+		if(str.equals(strCopy))
+			System.out.println("строка палиндром");
+		else
+			System.out.println("строка не явл палиндромом");
+		
+	}
+}
