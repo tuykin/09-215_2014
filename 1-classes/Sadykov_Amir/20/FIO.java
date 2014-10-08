2f6740094751d3db9a4ca5639a0b2753b534326a
amirka369 <amirka369@gmail.com>
2014-10-08 14:58:23 +0400

    create myClass
    class number 20
    version 1.1

---
+++ 1-classes/Sadykov_Amir/20/FIO.java
@@ -0,0 +1,29 @@
+package work2;
+
+public class FIO {
+	private String name;
+	private String surname;
+	private String patronymic;
+	//методы-конструкторы:
+	public FIO(){
+	
+	};
+	
+	public FIO(String name,String surname,String patronymic)
+	{
+		this.name=name;
+		this.surname=surname;
+		this.patronymic=patronymic;
+	}
+	//методы-селекторы:
+	public void Show()
+	{
+		System.out.println(this.name+" "+this.surname+" "+this.patronymic);
+	}
+	
+	public String getFIO()
+	{
+		return(this.name+" "+this.surname+" "+this.patronymic);
+	}
+	
+}
