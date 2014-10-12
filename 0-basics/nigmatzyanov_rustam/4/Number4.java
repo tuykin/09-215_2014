ea40afe317ab1c90f4409a182f40948372b77452
Nigmatzyanov <gre7if@gmail.com>
2014-10-13 00:48:22 +0400

    Create Number4.java

---
+++ 0-basics/nigmatzyanov_rustam/4/Number4.java
@@ -0,0 +1,30 @@
+package basics;
+import java.util.Scanner;
+
+public class Number4 {
+
+	public static int alg(int a,int b) {
+		System.out.print("Последовательность p,q: " + a + "," + b + '\t');
+		
+        while (b != 0) {
+            int temp = a%b;
+            a = b;
+            b = temp;
+            System.out.print(a + "," + b + '\t');
+        }
+        System.out.println();
+        return a;
+    }
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Введите два числа: ");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		
+		System.out.println("НОД: " + alg(a,b));
+		
+		sc.close();
+	}
+}
