b3226fad4e77dfcc4e9a59081ecf25f70ad46d51
Anvar Tuykin <tuykin@gmail.com>
2014-10-20 17:22:13 +0400

    Add UNIVERSITY db

---
+++ db/_course/create.sql
@@ -0,0 +1,19 @@
+PRAGMA foreign_keys=ON;
+
+CREATE TABLE departments (
+id INTEGER NOT NULL PRIMARY KEY,
+name VARCHAR NOT NULL
+);
+
+CREATE TABLE groups (
+id INTEGER NOT NULL PRIMARY KEY,
+department_id INTEGER REFERENCES departments(id),
+name VARCHAR NOT NULL
+);
+
+CREATE TABLE students (
+id INTEGER NOT NULL PRIMARY KEY,
+surname VARCHAR NOT NULL,
+name VARCHAR NOT NULL,
+group_id INTEGER
+);
