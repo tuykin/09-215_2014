877bdea71dfd8dcb2127e867012a3aa51841ff7c
Nigmatzyanov <gre7if@gmail.com>
2014-10-20 18:38:42 +0400

    Create newInserts.sql

---
+++ db/_course/newInserts.sql
@@ -0,0 +1,30 @@
+insert into departments values
+(1,'IVMiIT'),
+(2,'ITIS'),
+(3,'IFMiB');
+
+insert into groups values 
+(1,1,'921p', 0),
+(2,1,'922p', 0),
+(3,1,'923p', 0),
+(4,2,'14-304', 0),
+(5,2,'06-123', 0),
+(6,null,'06-123', 0);
+
+insert into students values
+(1,'Sadykov', 'Amir', 1),
+(2,'Ahmetshin', 'Robert', 1),
+(3,'Nabiullin', 'Adel', 1),
+(4,'Fattakhova', 'Yulduz', 2),
+(5,'Gimazutdinova', 'Zarina', 2),
+(6,'rtbhrt', 'bhrtbr', 3),
+(7,'btrbrbrht', 'brthbrt', 3),
+(8,'erbgrb', 'rthbrr', 4),
+(9,'ebbgebe', 'egvwevw', 4),
+(10,'gbwbwgbw', 'sgbdbvw', 5),
+(11,'wbtgbw', 'wtegbwe', 5),
+(12,'errehbe', 'erwh', 6),
+(13,'wgrgw', 'wgeewg', 6);
+
+
+
