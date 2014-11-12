a4ed536d81c7d66411f2e1ca46d96e50f0fb40b3
Anvar Tuykin <tuykin@gmail.com>
2014-11-12 13:13:41 +0300

    Create create_mysql.sql

---
+++ db/_course/create_mysql.sql
@@ -0,0 +1,23 @@
+USE `test`;
+
+DROP TABLE IF EXISTS departments;
+CREATE TABLE departments (
+id INTEGER NOT NULL PRIMARY KEY,
+name VARCHAR(45) NOT NULL
+);
+
+DROP TABLE IF EXISTS groups;
+CREATE TABLE groups (
+id INTEGER NOT NULL PRIMARY KEY,
+department_id INTEGER REFERENCES departments(id),
+name VARCHAR(45) NOT NULL,
+students_count INTEGER NOT NULL DEFAULT 0
+);
+
+DROP TABLE IF EXISTS students;
+CREATE TABLE students (
+id INTEGER NOT NULL PRIMARY KEY,
+surname VARCHAR(45) NOT NULL,
+name VARCHAR(45) NOT NULL,
+group_id INTEGER
+);
