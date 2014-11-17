93ecb5f4bbf19ec905bc42132760a737d7e07a27
Anvar Tuykin <tuykin@gmail.com>
2014-11-17 19:37:21 +0300

    Add stored procs to course db

--- db/_course/create.sql
+++ db/_course/create.sql
@@ -30,3 +30,56 @@ BEGIN
   WHERE groups.id = NEW.group_id;
 END;
 
+DELIMITER \\
+CREATE PROCEDURE test_proc()
+BEGIN
+SELECT *
+FROM students;
+END \\
+DELIMITER ;
+
+DELIMITER \\
+CREATE PROCEDURE count_students_in_groups()
+BEGIN
+SELECT g.name, COUNT(*)
+FROM students s INNER JOIN groups g ON g.id = s.group_id
+GROUP BY g.id
+END \\
+DELIMITER ;
+
+DELIMITER \\
+CREATE PROCEDURE select_students_count_by_group_name(IN group_name VARCHAR(45))
+BEGIN
+SELECT g.name, COUNT(*)
+FROM students s INNER JOIN groups g ON g.id = s.group_id
+GROUP BY g.id
+HAVING g.name LIKE CONCAT(group_name, '%');
+END \\
+DELIMITER ;
+
+DELIMITER \\
+CREATE PROCEDURE update_students_count_in_all_groups()
+BEGIN
+UPDATE groups g
+SET g.students_count = (
+    SELECT COUNT(*)
+    FROM students s
+    GROUP BY s.group_id
+    HAVING s.group_id = g.id
+    );
+END \\
+DELIMITER ;
+
+DELIMITER \\
+CREATE PROCEDURE update_students_count_in_group(IN group_id_param INTEGER)
+BEGIN
+UPDATE groups g
+SET g.students_count = (
+    SELECT COUNT(*)
+    FROM students s
+    GROUP BY s.group_id
+    HAVING s.group_id = g.id
+    )
+WHERE g.id = group_id_param;
+END \\
+DELIMITER ;
