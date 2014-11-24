c77b5db3dbd4fb7c2f2abb73e1317975b29b4aaa
amirka369 <amirka369@gmail.com>
2014-11-25 01:27:19 +0300

    add "console"DB (win forms)

---
+++ consoleDB/Sadykov_Amir/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ShowTables
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(317, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(127, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(266, 184);
+            this.textBox1.TabIndex = 1;
+           // this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 262);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ consoleDB/Sadykov_Amir/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ShowTables
+{
+    public partial class Form1 : Form
+    {
+        String ConStr;
+        public Form1()
+        {
+            InitializeComponent();
+            GetTablesNames();
+        }
+
+        MySqlConnection myConnectionOpen()
+        {
+            ConStr = "CharSet=utf8;Database=gai;Data Source=127.0.0.1;User Id=root;Password='1234'";
+            MySqlConnection con = new MySqlConnection(ConStr);
+            try
+            {
+                con.Open();
+                return con;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Подключение не установлено! ");
+                return null;
+            }
+        }
+
+        private void GetTablesNames()
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.CommandText = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA='gai';";
+            MySqlConnection con = myConnectionOpen();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            MySqlDataReader reader = null;
+
+            try
+            {
+                reader = cmd.ExecuteReader();
+            }
+            catch (Exception f)
+            {
+                MessageBox.Show(f.Message);
+            }
+
+            if (reader != null)
+            {
+
+                StringBuilder str = new StringBuilder();
+                while (reader.Read())
+                {
+                    str.Append(reader[0].ToString());
+                    str.Append("\n");
+                    comboBox1.Items.Add(reader[0].ToString());
+                }
+            }
+            con.Close();
+        }
+
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.CommandText = "SELECT * FROM " + comboBox1.Text + ";";
+            MySqlConnection con = myConnectionOpen();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            MySqlDataReader reader = null;
+            try
+            {
+                reader = cmd.ExecuteReader();
+            }
+            catch (Exception f)
+            {
+                MessageBox.Show(f.Message);
+            }
+
+            if (reader != null)
+            {
+
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        textBox1.Text += reader[i].ToString();
+                        textBox1.Text += " ";
+                    }
+                    textBox1.Text += Environment.NewLine;
+                }
+            }
+            con.Close();
+        }
+
+    }
+}
