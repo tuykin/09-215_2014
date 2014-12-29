ffa29ef34ec1c0e8e931a24d9ff58c9994ba27c0
Ksenia <besedina_ksenia@bk.ru>
2014-12-29 13:22:51 +0300

    New

--- db/Besedina_Ksenia/CREATE_new.sql
+++ db/Besedina_Ksenia/CREATE_new.sql
@@ -5,22 +5,22 @@ USE `tableapp`;
 CREATE TABLE customers(
 login VARCHAR(45) NOT NULL PRIMARY KEY,
 password VARCHAR(45) NOT NULL,
-e_mail VARCHAR(45) NOT NULL,
-t_number VARCHAR(45) NOT NULL,
+e_mail VARCHAR(45),
+t_number VARCHAR(45),
 surname VARCHAR(45) NOT NULL,
 name VARCHAR(45) NOT NULL,
-patronymic VARCHAR(45),
+patronymic VARCHAR(45) NOT NULL,
 address VARCHAR(45) NOT NULL,
-date_of_b DATE NOT NULL
+date_of_b DATE
 );
 
 
 CREATE TABLE delivery(
 id_orders INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-sum_weight FLOAT NOT NULL DEFAULT 0,
-sum_volume FLOAT NOT NULL DEFAULT 0,
+sum_weight FLOAT UNSIGNED NOT NULL DEFAULT 0,
+sum_volume FLOAT UNSIGNED NOT NULL DEFAULT 0,
 date_delivery DATE NOT NULL,
-price_delivery DECIMAL(7,2) NOT NULL,
+price_delivery DECIMAL(7,2) UNSIGNED NOT NULL,
 state VARCHAR(45) NOT NULL,
 district VARCHAR(45) NOT NULL,
 type_delivery VARCHAR(45) NOT NULL
@@ -50,11 +50,11 @@ e_mail VARCHAR(45) NOT NULL
 CREATE TABLE goods (
   id_gds INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name_gds VARCHAR(45) NOT NULL,
-  price DECIMAL(7,2) NOT NULL,
-  weight FLOAT NOT NULL,
-  length FLOAT NOT NULL,
-  width FLOAT NOT NULL,
-  height FLOAT NOT NULL,
+  price DECIMAL(7,2) UNSIGNED NOT NULL,
+  weight FLOAT UNSIGNED NOT NULL,
+  length FLOAT UNSIGNED NOT NULL,
+  width FLOAT UNSIGNED NOT NULL,
+  height FLOAT UNSIGNED NOT NULL,
   category VARCHAR(45) NOT NULL,
   subcategory VARCHAR(45) NOT NULL,
   name_creator VARCHAR(45) NOT NULL,
@@ -75,19 +75,12 @@ id_orders INTEGER  AUTO_INCREMENT PRIMARY KEY NOT NULL,
 login VARCHAR(45) NOT NULL,
 type_pay VARCHAR(45) NOT NULL,
 type_receipt VARCHAR(45) NOT NULL,
-sum_price DECIMAL(7,2) NOT NULL DEFAULT 0 ,
+sum_price DECIMAL(7,2) UNSIGNED NOT NULL DEFAULT 0 ,
 date DATE  NOT NULL,
 state VARCHAR(15)  NOT NULL,
 name_point VARCHAR(45),
 FOREIGN KEY (name_point) REFERENCES point(name_point),
 FOREIGN KEY (login) REFERENCES customers(login)
-);
-
-CREATE TABLE goods_orders(
-id_gds INTEGER NOT NULL,
-id_orders  INTEGER NOT NULL,
-FOREIGN KEY (id_gds) REFERENCES goods(id_gds),
-FOREIGN KEY (id_orders) REFERENCES orders(id_orders)
 );
 
 CREATE TABLE e_wallet(
@@ -96,14 +89,11 @@ login VARCHAR(45) NOT NULL,
 FOREIGN KEY (login) REFERENCES customers(login)
 );
 
-
-
-DROP TABLE IF EXISTS e_wallet;
-DROP TABLE IF EXISTS point;
-DROP TABLE IF EXISTS customers;
-DROP TABLE IF EXISTS delivery;
-DROP TABLE IF EXISTS credit_card;
-DROP TABLE IF EXISTS procreator;
-DROP TABLE IF EXISTS goods;
-DROP TABLE IF EXISTS goods_orders;
-DROP TABLE IF EXISTS orders;
+CREATE TABLE goods_orders(
+id_gds INTEGER,
+id_orders INTEGER,
+login VARCHAR(45) NOT NULL,
+FOREIGN KEY (id_gds) REFERENCES goods(id_gds),
+FOREIGN KEY (id_orders) REFERENCES orders(id_orders),
+FOREIGN KEY (login) REFERENCES customers(login)
+);
